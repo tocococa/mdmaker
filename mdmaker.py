b8ce1a83de32097792d8c4cee73e49387cb5677d
tocococa <tocococa@gmail.com>
2021-03-15 18:42:51 -0300

    autorun

--- mdmaker.py
+++ mdmaker.py
@@ -1,7 +1,14 @@
 import os
 from datetime import date
 
-path = os.path.join(os.getcwd(), str(date.today()))
 
-with open(path + '.md', 'w') as file:
-    pass
+def main():
+    path = os.path.join(os.getcwd(), str(date.today()))
+
+    with open(path + '.md', 'w') as file:
+        pass
+
+
+if __name__ == '__main__':
+    main()
+    
